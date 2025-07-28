701e4849f7fc862812f5c02ea76a014161de7d1d
konchu974 <156214774+konchu974@users.noreply.github.com>
2025-07-28 11:23:45 +0200

    debut jwt

---
+++ src/main/java/fr/afpa/orm/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package fr.afpa.orm.repositories;
+
+import fr.afpa.orm.entities.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
